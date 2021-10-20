aa63ffd059d81e3acbe7c91e72dadb61b2100f7f
noritsune <bill.gatess4037@gmail.com>
2021-10-20 12:34:56 +0900

    refactor: 読みやすいように処理順を変更

--- scripts/RotateCube.js
+++ scripts/RotateCube.js
@@ -9,6 +9,7 @@ function init() {
 	
 	// 2. カメラを初期化
 	const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	camera.position.z = 5;
 
 	// 3. 描画先のDOM要素を生成
 	const renderer = new THREE.WebGLRenderer();
@@ -21,16 +22,13 @@ function init() {
 	const cube = new THREE.Mesh( geometry, material );
 	scene.add( cube );
 
-	camera.position.z = 5;
-
 	const animate = function () {
-		requestAnimationFrame( animate );
-
 		cube.rotation.x += 0.01;
 		cube.rotation.y += 0.01;
-
+		
 		renderer.render( scene, camera );
+		requestAnimationFrame( animate );
 	};
-
+	
 	animate();
 }
